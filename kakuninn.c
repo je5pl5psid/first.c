fe051ab9387a94c63d136f512d044dfbb2b82a30
je5pl5psid <ka.201806.to@gmail.com>
2022-06-15 17:13:09 +0900

    add retry kakuninn.c

--- kakuninn.c
+++ kakuninn.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-    printf("Hello\t World\nkonami\tkann");
-    
+    int money,iti,sann,go,hati;
+    printf("’è‰¿‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+    scanf("%d",&money);
+    iti = (int)(money * 0.9);
+    sann = (int)(money * 0.7);
+    go = (int)(money * 0.5);
+    hati = (int)(money * 0.2);
+    printf("ˆêŠ„ˆø‚ÍA%d‰~\nOŠ„ˆø‚ÍA%d‰~\nŒÜŠ„ˆø‚ÍA%d‰~\n”ªŠ„ˆø‚ÍA%d‰~\n",iti,sann,go,hati);
     return 0;
 }
