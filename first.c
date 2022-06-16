40ff36fa5f8a05186cb812b959651e1e9896c086
je5pl5psid <ka.201806.to@gmail.com>
2022-06-16 18:11:49 +0900

    add sentence else

--- first.c
+++ first.c
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-    int suuti = 10;
-    if (suuti)
-        printf("%d\n", suuti);
+    int suuti ;
+    scanf("%d", &suuti);
+    if (suuti==10)
+        printf("this is 10");
+    else
+    printf("not 10");
     return 0;
 }
