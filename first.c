30ac7b84ca31c8feb64ee3099c211297afb75b52
je5pl5psid <ka.201806.to@gmail.com>
2022-06-18 11:31:18 +0900

    original?answer

--- first.c
+++ first.c
@@ -4,11 +4,10 @@
 int main(void)
 {
     int suuti,amari;
-    printf("年を入力してください\n");
     scanf("%d",&suuti);
-    //amari = suuti % 4;
-    if (suuti % 4 == 0)printf("夏のオリンピック");
-    else if(suuti % 4 == 2)printf("冬のオリンピック");
+    amari = suuti % 4;
+    if (amari == 0)printf("夏のオリンピック");
+    else if(amari == 2)printf("冬のオリンピック");
     else printf("無しオリンピック");
     
     return 0;
