8b0ed0fd256f9e85b3d0de9ee9055f997ebfd5a1
je5pl5psid <ka.201806.to@gmail.com>
2022-06-18 10:22:22 +0900

    missed

--- first.c
+++ first.c
@@ -1,8 +1,14 @@
+
 #include <stdio.h>
 
 int main(void)
 {
-    int suuti ;
+    int suuti,amari;
     scasnf("%d",&suuti);
-    if (suuti == 2*)
+    amari = suuti % 4;
+    if (amari == 0)printf("夏のオリンピック");
+    //else if(amari == 2)printf("冬のオリンピック");
+   // else ptintf("無しオリンピック");
+    
+    return 0;
 }
