5f6d88562b054822974e73e5b086213fcc254dfc
je5pl5psid <ka.201806.to@gmail.com>
2022-06-18 14:06:12 +0900

    change quiz and missed

--- first.c
+++ first.c
@@ -1,14 +1,16 @@
-
 #include <stdio.h>
 
 int main(void)
 {
-    int suuti,amari;
-    scanf("%d",&suuti);
-    amari = suuti % 4;
-    if (amari == 0)printf("夏のオリンピック");
-    else if(amari == 2)printf("冬のオリンピック");
-    else printf("無しオリンピック");
-    
-    return 0;
+
+
+int a , b , c , i;
+a=1;
+b=1;
+for(i = 1 ;  i < 10; i++){
+c=a+b;
+
+printf("%05d回目 %d\n",i,c);
+
+}
 }
