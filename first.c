d971fc597aaf4a02138056b58599e9c948478dd2
je5pl5psid <ka.201806.to@gmail.com>
2022-06-19 00:24:33 +0900

    renewal

--- first.c
+++ first.c
@@ -6,21 +6,10 @@ int main(void){
     b = 1;
     printf("put inn suuti\n");
     scanf("%d",&kazu);
-    kazu = kazu - 1;
-    for(i = 1 ;  i <= kazu; i++){
+    for(i = 1 ;  i < kazu; i++){
         c = b + a;
         a = b;
         b = c;
     }
      printf("%05dtimes %d\n",i,b);
-return 0;
-    for(int i = 1;i <= 9; i++)
-    {
-        for(int j = 1; j <= 9; j++)
-        {
-            printf("%02d ",i * j);
-        }
-        printf("\n");
-    }
-    return 0;
 }
