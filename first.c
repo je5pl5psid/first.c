c8dfbb54f93721aa4e6554a1ee8bdb291a08b9f0
je5pl5psid <ka.201806.to@gmail.com>
2022-08-04 15:05:30 +0900

    rennndyuu14

--- first.c
+++ first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int i ,array1[10];
-    printf("10ko no suujiwo , dekugitte kaku:");
-    for(i = 0; i < sizeof(array1) / sizeof(array1[0]) ; i++){
-       scanf ("%d,",&array1[i]);
-    }
-    for(i = (sizeof(array1) / sizeof(array1[0])-1) ; i >= 0 ; i--){
-        printf("number[%d] :%d\n",i,array1[i]);
-    }
+    char c[64];
+    char d[32];
+    printf ("put in first name:");
+    scanf("%32s",c);
+    printf("put in second name:");
+    scanf("%64s",d);
+    printf("%s %s" ,c,d);
     return 0;
+
+
 }
