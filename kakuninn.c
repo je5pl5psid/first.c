ae7b0439b24271ef1afa357fa3bea5cf295538b8
je5pl5psid <ka.201806.to@gmail.com>
2022-08-04 10:10:50 +0900

    kakuninn2kaime

--- kakuninn.c
+++ kakuninn.c
@@ -2,13 +2,34 @@
 
 int main(void)
 {
+    int ei;
+    ei = 0;
+    if
+    (ei == 0){
+        printf("sumer");
+    }
+    else if
+    (ei == 2){
+        printf("winter");
+    }
+    else if 
+    (ei == 1){
+        printf("nasi");
+    }
+    else if
+    (ei == 3){
+        printf("nasi");
+    }
+    return 0;
+    /*
     int money,iti,sann,go,hati;
-    printf("’è‰¿‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+    printf("put in money");
     scanf("%d",&money);
     iti = (int)(money * 0.9);
     sann = (int)(money * 0.7);
     go = (int)(money * 0.5);
     hati = (int)(money * 0.2);
-    printf("ˆêŠ„ˆø‚ÍA%d‰~\nOŠ„ˆø‚ÍA%d‰~\nŒÜŠ„ˆø‚ÍA%d‰~\n”ªŠ„ˆø‚ÍA%d‰~\n",iti,sann,go,hati);
+    printf("10%waribiki...%dyen\n30%waribiki...%dyen\n50%waribiki...%dyen\n80%waribiki...%dyen\n",iti,sann,go,hati);
     return 0;
+    */
 }
