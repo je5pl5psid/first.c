e177371b7d8349ec29c39d2b0a4ce33c27374d1f
je5pl5psid <ka.201806.to@gmail.com>
2022-08-17 16:19:52 +0900

    kakuninn

--- first.c
+++ first.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-int main(void)
-{
-    char c[64];
-    char d[32];
-    printf ("put in first name:");
-    scanf("%32s",c);
-    printf("put in second name:");
-    scanf("%64s",d);
-    printf("%s %s" ,c,d);
+
+int main(void){
+    int hu = 10;
+    printf("%d\noisiigohann\n",hu);
     return 0;
-
-
 }
