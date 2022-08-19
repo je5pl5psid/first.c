3d635dcf55981073733170ff4a87116d33466752
je5pl5psid <ka.201806.to@gmail.com>
2022-08-19 15:20:32 +0900

    toi15nosippai

--- first.c
+++ first.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(void){
-    int hu = 10;
-    printf("%d\noisiigohann\n",hu);
+int main(void)
+{
+    int i,j,moji[256];
+    for(i = 1 ; ; i ++){ 
+        printf("suuji wo nyuuryoku site kudasai:");
+        scanf("%d",&j);
+        if (j == -1) break ;
+        moji[i] = j ;
+        printf("%s\n",moji);
+    }
     return 0;
 }
