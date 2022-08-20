604b9829559e8aa83be536b23d2930c60caeffdc
je5pl5psid <ka.201806.to@gmail.com>
2022-08-20 13:53:20 +0900

    kakuninn

--- first.c
+++ first.c
@@ -6,9 +6,17 @@ int main(void)
     for(i = 1 ; ; i ++){ 
         printf("suuji wo nyuuryoku site kudasai:");
         scanf("%d",&j);
+        for(;;)
+        {
+            if (j >= -1 && j <= 100) break;
+            printf("suuji wo nyuuryoku site kudasai:");
+            scanf("%d",&j);
+
+        }
         if (j == -1) break ;
         moji[i] = j ;
         printf("%s\n",moji);
     }
+    printf("owari");
     return 0;
-}
+} 
