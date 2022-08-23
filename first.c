e714a484fa8cb14e054f1b36fca20b0b6df1fecf
je5pl5psid <ka.201806.to@gmail.com>
2022-08-23 11:01:55 +0900

    hairetu totyuu

--- first.c
+++ first.c
@@ -2,22 +2,31 @@
 
 int main(void)
 {
-    printf("check");
-    int i,j,moji[256];
-    for(i = 1 ; ; i ++){ 
+    int i,j,k,l,moji[10] ={0,0,0,0,0,0,0,0,0,0};
+    for(i = 0 ; i < 10; i ++)
+    { 
         printf("suuji wo nyuuryoku site kudasai:");
         scanf("%d",&j);
+        printf("%d\n",j);
         for(;;)
         {
             if (j >= -1 && j <= 100) break;
-            printf("suuji wo nyuuryoku site kudasai:");
+            printf("suujiwonyuuryokusitekudasai:");
             scanf("%d",&j);
-
         }
-        if (j == -1) break ;
-        moji[i] = j ;
-        printf("%s\n",moji);
+    moji[i]=j;
+    if (j == -1) break ;
+    for (k = 0; k < 10; k++)
+    {
+        printf("moji[%d] = %d\n", k, moji[k]);
+        }
     }
-    printf("owari");
+    printf("owari\n");
+    for (l = 0; l < 10; l++)
+    {
+        k = 1;
+        printf("moji[%d]bannmeha: %d\n", k, moji[l]);
+        k = k + 1;
+        }
     return 0;
-} 
+    }
