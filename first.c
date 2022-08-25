b0eca736974ff16eac938f039643eede5bbb988c
je5pl5psid <ka.201806.to@gmail.com>
2022-08-25 16:36:04 +0900

    16totyuu

--- first.c
+++ first.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-//suuti nyuuryoku de "25","26","27"atari de sakaime no nanika ga arimasu.
+typedef struct 
+{
+    char name[64];
+    int age;
+    char gender[1];//M = man;F = women;also LGBT...+
+} person;
 
-int keisann(int data[10],int *min,int *max);
+void person_print(person data);
 
-int main(void){
-    int i,j,k,l,kekka,moji[10] ={-2,-2,-2,-2,-2,-2,-2,-2,-2,-2};
-    int min;
-    int max;
-    for(i = 0 ; i < 10; i ++){ 
-        printf("[%d]suuji wo nyuuryoku site kudasai:",i+1);
-        scanf("%d",&j);
-        for(;;){
-            if (j >= -1 && j <= 100) break;
-            printf("suujiwonyuuryokusitekudasai:");
-            scanf("%d",&j);
-        }
-    moji[i]=j;
-    if (j == -1) break ;
-    }
-keisann(moji,&min,&max);
-printf("saisyou[%d] saidai[%d]",min,max);
+int main(void)
+{
+    person data;
+
+    data.age = 18;
+    strcpy(data.gender,"M");
+    strcpy(data.name,"GonNGITUNE");
+
+    person_print(data);
+    return 0;
 }
 
-
-
-
-
-int keisann(int data[10],int *min,int *max){
-    int i;
-    *min = 100;
-    *max = 0;
-    for(i = 0;i <= 9;i ++){
-        if(data[i] == -1)break;
-        if(data[i] < *min) *min = data[i];
-        if(data[i] > *max) *max = data[i];
-    }
+void person_print(person data)
+{
+    printf("[name]:%s\n",data.name);
+    printf("[age]:%d\n",data.age);
+    printf("[gender]:%s\n",data.gender);
+    return ;
 }
