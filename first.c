843f5531518a2338d12cbaa686c0e34376f7b0f6
je5pl5psid <ka.201806.to@gmail.com>
2022-08-29 13:55:46 +0900

    16sippai

--- first.c
+++ first.c
@@ -8,24 +8,34 @@ typedef struct
     char gender[1];//M = man;F = women;also LGBT...+
 } person;
 
-void person_print(person data);
+void person_print(person data)
+void person_s(person *data)
 
-int main(void)
-{
-    person data;
-
-    data.age = 18;
-    strcpy(data.gender,"M");
-    strcpy(data.name,"GonNGITUNE");
-
-    person_print(data);
+int main(void){
+    person data[3];
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+    person_s(&data[i]);
+    person_print(data[i]);
+    }
     return 0;
 }
 
-void person_print(person data)
-{
+void person_print(person data){
     printf("[name]:%s\n",data.name);
     printf("[age]:%d\n",data.age);
     printf("[gender]:%s\n",data.gender);
+
+}
+
+void person_s(person *data){
+    printf("nennreiwonyuuryokusitekudasai:\n");
+    scanf("%d",&data->age);
+    printf("namaewonyuuryokusitekudasai:\n");
+    scanf("%s",data->name);
+    printf("M=man,F=woman,another:\n");
+    scanf("%s",data->gender);
+    printf("\n");
     return ;
 }
