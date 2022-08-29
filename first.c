2b52734cf62c89a89f618eb45a715e4a6a1904d6
je5pl5psid <ka.201806.to@gmail.com>
2022-08-29 16:16:20 +0900

    16hitotudake

--- first.c
+++ first.c
@@ -8,33 +8,37 @@ typedef struct
     char gender[2];  
 } person ;
 
-void person_scan(person *data);
-void person_print(person *data);
+void person_scan(person *data,int j);
+void person_print(person *data,int k);
 
 int main(void)
 {
     person data[3];
-    person_scan(data);//yobidasi
-//    strcpy(data[1].name,"wawawa");
-//    data[1].age = 35;
-//    strcpy(data[1].gender,"M");
-    person_print(data);//yobidasi  //配列名-> ポインタ
+    int i,l;
+    for(i = 0;i < 3;i++)
+    {
+        person_scan(data,i);
+    }
+    for(l = 0;l < 3;l++)
+    {
+    person_print(data,l);//yobidasi  //配列名-> ポインタ
+    }
     return 0;
 }
 
-void person_scan(person *data)
+void person_scan(person *data,int j)
 {
-    printf("名前を入力してください");
-    scanf("%s",&data[1].name);
-    printf("年齢を入力してください");
-    scanf("%d",&data[1].age);
-    printf("性別M=man,F=woman");
-    scanf("%s",&data[1].gender);
+    printf("名前を入力してください[%d]",j);
+    scanf("%s",&data[j].name);
+    printf("年齢を入力してください[%d]",j);
+    scanf("%d",&data[j].age);
+    printf("性別M=man,F=woman[%d]",j);
+    scanf("%s",&data[j].gender);
 }
 
-void person_print(person *data)
+void person_print(person *data,int k)
 {
-    printf("name  [%s]\n",data[1].name);
-    printf("age   [%d]\n",data[1].age);
-    printf("gender[%s]\n",data[1].gender);
+    printf("name[%d]%s\n",k,data[k].name);
+    printf("age   %d\n",data[k].age);
+    printf("gender%s\n",data[k].gender);
 }
