f549315f6aff7ba4a700e7b15a5ffe4d2f160be7
je5pl5psid <ka.201806.to@gmail.com>
2022-09-01 15:25:32 +0900

    hukusyuu

--- first.c
+++ first.c
@@ -1,44 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef struct
-{
-    char name[256];
-    int age;
-    char gender[2];  
-} person ;
-
-void person_scan(person *data,int j);
-void person_print(person *data,int k);
 
 int main(void)
 {
-    person data[3];
-    int i,l;
-    for(i = 0;i < 3;i++)
-    {
-        person_scan(data,i);
-    }
-    for(l = 0;l < 3;l++)
-    {
-        person_print(data,l);
-    }
+    int milk ,water,money ,change ,kane;
+    milk = 138;
+    water = 198;
+    money = (milk*2 + water)*1.05;
+    kane = 1000;
+    printf("%d",kane - money);
     return 0;
-}
-
-void person_scan(person *data,int j)
-{
-    printf("–¼‘O‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[%d]",j);
-    scanf("%s",&data[j].name);
-    printf("”N—î‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[%d]",j);
-    scanf("%d",&data[j].age);
-    printf("«•ÊM=man,F=woman[%d]",j);
-    scanf("%s",&data[j].gender);
-}
-
-void person_print(person *data,int k)
-{
-    printf("name[%d]%s\n",k,data[k].name);
-    printf("age    %d\n",data[k].age);
-    printf("gender %s\n",data[k].gender);
 }
